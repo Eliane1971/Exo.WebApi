00462e7b7493d87b03317be452868f90e546b364
Eliane Ribeiro <egmribeiro@gmail.com>
2024-09-25 17:17:29 -0300

    segundo comit

--- obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
+++ obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6945435ee0ac89c242e412599ea70c19a5f7e1ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3324292fd5020a5144f53788f51b156341369b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
